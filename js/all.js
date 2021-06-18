5358d7501a4000bdd74af065337e0bf5bbba188e
simon779552 <simon779552@gmail.com>
2021-06-18 20:57:37 +0800

    js

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
 $(function() {
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) { /* 要滑動到選單的距離 */
+        if ($(this).scrollTop() > 50) { /* 要滑動到選單的距離 */
             $('.drop-down-menu').addClass('navFixed'); /* 幫選單加上固定效果 */
         } else {
             $('.drop-down-menu').removeClass('navFixed'); /* 移除選單固定效果 */
