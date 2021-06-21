604ed0a51c60ee71e2f8453a1f91384a078bd255
simon779552 <simon779552@gmail.com>
2021-06-22 01:57:06 +0800

    updata

--- js/all.js
+++ js/all.js
@@ -1,9 +1,9 @@
 $(function() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) { /* 要滑動到選單的距離 */
-            $('.drop-down-menu').addClass('navFixed'); /* 幫選單加上固定效果 */
+            $('.drop-down-menu').addClass('nav-fixed'); /* 幫選單加上固定效果 */
         } else {
-            $('.drop-down-menu').removeClass('navFixed'); /* 移除選單固定效果 */
+            $('.drop-down-menu').removeClass('nav-fixed'); /* 移除選單固定效果 */
         }
     });
 });
